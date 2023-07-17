1b71b7f31d37f498a959174ef88c3c06ed9d10aa
Садыков Ранил <sri@vt2b.ru>
2023-07-17 10:22:31 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "@/app/styles/Home.module.sass";
+
+export default function Home() {
+  return <main className={styles.main}>Hello World!</main>;
+}
